6f77367def443240b39da7222dd42cc4c7b370c9
Giulio Sorrentino <numerone@fastwebnet.it>
2026-09-24 22:30:29 +0200

    cambiato provider di icone da optris a google.

--- WheelOfNumFortune/App.axaml.cs
+++ WheelOfNumFortune/App.axaml.cs
@@ -1,8 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Optris.Icons.Avalonia;
-using Optris.Icons.Avalonia.MaterialDesign;
 using WheelOfNumFortune.ViewModels;
 using WheelOfNumFortune.Views;
 
@@ -13,7 +11,6 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
-        IconProvider.Current.Register<MaterialDesignIconProvider>();
     }
 
     public override void OnFrameworkInitializationCompleted()
